7b9387db3ec90fffbe649034877d175c26e0e1a3
mwisniewski00 <macwis162@gmail.com>
2022-11-21 19:53:58 +0100

    Fix after rebase errors

--- client/src/chessgame/src/game/MovesGenerator.ts
+++ client/src/chessgame/src/game/MovesGenerator.ts
@@ -4,7 +4,7 @@ import { mapColumnIndexToLetter } from "../utils";
 
 export class MovesGenerator {
   private gameState: GameState;
-  private startingPawnIndexes = { [Colors.WHITE]: 6, [Colors.BLACK]: 1 };
+  private startingPawnIndexes = { [Colors.BLACK]: 6, [Colors.WHITE]: 1 };
 
   constructor(gameState: GameState) {
     this.gameState = gameState;
@@ -23,7 +23,7 @@ export class MovesGenerator {
     column: number,
     color: Colors,
   ): string[] {
-    const colorMultiplier = color === Colors.WHITE ? -1 : 1;
+    const colorMultiplier = color === Colors.BLACK ? -1 : 1;
     const possibleMoves: string[] = [];
     const enemyColor = this.getEnemyColor(color);
     const nextRow = row + colorMultiplier * 1;
@@ -179,13 +179,66 @@ export class MovesGenerator {
     }
   }
 
-  public getAllPossibleMoves() {
+  private areFieldsBetweenPiecesInRowEmpty(
+    row: number,
+    column1: number,
+    column2: number,
+  ) {
+    const fieldsToCheck = this.gameState[row].slice(column1, column2);
+    return fieldsToCheck.every(field => !field);
+  }
+
+  private addCastlingMoves(
+    allPossibleMoves: object,
+    castlingAvailability: string,
+    movesNext: Colors,
+  ) {
+    const movesToUpdate = { ...allPossibleMoves };
+    if (castlingAvailability.length) {
+      const rowToCheck = movesNext === Colors.WHITE ? 0 : 7;
+      const possibleEnemyMoves = this.getAllPossibleBasicMoves(
+        this.getEnemyColor(movesNext),
+      );
+      const isQueenSidePossible =
+        castlingAvailability.includes(movesNext === Colors.WHITE ? "Q" : "q") &&
+        this.areFieldsBetweenPiecesInRowEmpty(rowToCheck, 1, 4) &&
+        !this.willFieldBeAttacked(`c${rowToCheck + 1}`, possibleEnemyMoves);
+      const isKingSidePossible =
+        castlingAvailability.includes(movesNext === Colors.BLACK ? "K" : "k") &&
+        this.areFieldsBetweenPiecesInRowEmpty(rowToCheck, 5, 7) &&
+        !this.willFieldBeAttacked(`g${rowToCheck + 1}`, possibleEnemyMoves);
+
+      const rookColumns = [
+        ...(isQueenSidePossible ? ["a"] : []),
+        ...(isKingSidePossible ? ["h"] : []),
+      ];
+      const kingField = `e${rowToCheck + 1}`;
+      rookColumns.forEach(column => {
+        const rookField = `${column}${rowToCheck + 1}`;
+        movesToUpdate[rookField] = [
+          ...((movesToUpdate[rookField] || []) as string[]),
+          kingField,
+        ];
+        movesToUpdate[kingField] = [
+          ...((movesToUpdate[kingField] || []) as string[]),
+          rookField,
+        ];
+      });
+    }
+    return movesToUpdate;
+  }
+
+  private getAllPossibleBasicMoves(moves: Colors) {
     const allPossibleMoves = {};
     this.gameState.forEach((row, row_index) => {
       row.forEach((field, column_index) => {
         const piecePosition = `${mapColumnIndexToLetter(column_index)}${
           row_index + 1
         }`;
+        if (field?.color !== moves) {
+          allPossibleMoves[piecePosition] = [];
+          return;
+        }
         allPossibleMoves[piecePosition] = this.getPossibleMoves(
           row_index,
           column_index,
@@ -194,5 +247,16 @@ export class MovesGenerator {
       });
     });
     return allPossibleMoves;
+  }
+
+  private willFieldBeAttacked(field: string, enemyMoves: object) {
+    return Object.values(enemyMoves).some(moves =>
+      (moves as string[]).includes(field),
+    );
+  }
+
+  public getAllPossibleMoves(movesNext: Colors, castlingAvailability: string) {
+    const basicMoves = this.getAllPossibleBasicMoves(movesNext);
+    return this.addCastlingMoves(basicMoves, castlingAvailability, movesNext);
   }
 }
