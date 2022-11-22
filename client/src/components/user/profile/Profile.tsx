75f5178f0139b05b0408c9a3416673aaaf31931d
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-11-22 19:39:29 +0100

    Basic profile page

--- client/src/components/user/profile/Profile.tsx
+++ client/src/components/user/profile/Profile.tsx
@@ -1,5 +1,22 @@
+import { url } from "inspector";
+import "./Profile.scss"
+
 const Profile: React.FC = () => {
-        return <div>Profile</div>;
+  return (
+    <div className="profile-page">
+      <div className="profile-content">
+        <div className="profile-header">
+          <div className="profile-avatar" style={{backgroundImage: 'url("https://cdn.dribbble.com/users/1078367/screenshots/7128092/c_for_chess_logo-100_4x.jpg")'}}></div>
+          <div className="profile-name">
+            Admin
+          </div>
+          <a className="profile-edit">Edit</a>
+        </div>
+        <div className="profile-statistics"></div>
+        <div className="profile-game-history"></div>
+      </div>
+    </div>
+  )
 }
 
 export default Profile;
