f09ae1914bd37065943eb90c97465e980ec92604
Azalurg <91213389+Azalurg@users.noreply.github.com>
2023-01-08 16:42:09 +0100

    Small changes in profile page

--- client/src/components/user/profile/Profile.tsx
+++ client/src/components/user/profile/Profile.tsx
@@ -1,19 +1,21 @@
 import useAxiosPrivate from "hooks/useAxiosPrivate";
 import axios from "api/axios";
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import { useEffect, useState } from "react";
 import IUser from "../../../models/IUser";
-import "./Profile.scss"
+import "./Profile.scss";
 import useUserModal from "hooks/useUserModal";
-import {EditUserModal} from "../modals/edit/EditUserModal";
+import { EditUserModal } from "../modals/edit/EditUserModal";
 import { useNavigate } from "react-router-dom";
-import EditIcon from '@mui/icons-material/Edit';
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import ScoreboardIcon from '@mui/icons-material/Scoreboard';
+import EditIcon from "@mui/icons-material/Edit";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import ScoreboardIcon from "@mui/icons-material/Scoreboard";
+import useAuth from "hooks/useAuth";
+import EditDescriptionModal from "../modals/edit/EditDescriptionModal";
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -25,9 +27,15 @@ function TabPanel(props: TabPanelProps) {
   const { children, value, index, ...other } = props;
 
   return (
-    <div role="tabpanel" className="profile-tab-content" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
+    <div
+      role="tabpanel"
+      className="profile-tab-content"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
       {children}
-    
     </div>
   );
 }
@@ -35,7 +43,7 @@ function TabPanel(props: TabPanelProps) {
 function a11yProps(index: number) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
@@ -43,64 +51,88 @@ function calculateTime(isoString: string) {
   const date = new Date(isoString);
   const now = new Date();
   const timeSince = now.getTime() - date.getTime();
-  if (timeSince < 60000) { // Less than a minute
+  if (timeSince < 60000) {
+    // Less than a minute
     return `${Math.round(timeSince / 1000)} seconds`;
   }
-  if (timeSince < 3600000) { // Less than an hour
+  if (timeSince < 3600000) {
+    // Less than an hour
     return `${Math.round(timeSince / 60000)} minutes`;
   }
-  if (timeSince < 86400000) { // Less than a day
+  if (timeSince < 86400000) {
+    // Less than a day
     return `${Math.round(timeSince / 3600000)} hours`;
   }
-  if (timeSince < 2592000000) { // Less than a month
+  if (timeSince < 2592000000) {
+    // Less than a month
     return `${Math.round(timeSince / 86400000)} days`;
   }
   return `${Math.round(timeSince / 2592000000)} months`;
 }
 
-
-
 const Profile: React.FC = () => {
   const [user, setUser] = useState({} as IUser);
   const [value, setValue] = useState(0);
   const axiosPrivate = useAxiosPrivate();
-  const { isOpen, setIsOpen } = useUserModal();
+  const { isOpen: isOpenDescription, setIsOpen: setIsOpenDescription } = useUserModal();
   const navigate = useNavigate();
-
-  
-
+  const auth = useAuth().auth;
+  const loggedInUser = auth.username;
+  const username = window.location.pathname.slice(9);
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
   useEffect(() => {
     async function getUser() {
-      try{
-        const username = window.location.pathname.slice(9)
-        const response = await axios.get(`users/profile/${username}`)
-        if(response.status === 200) setUser(response.data);
-      } catch(err){
+      try {
+        const response = await axios.get(`users/profile/${username}`);
+        if (response.status === 200) setUser(response.data);
+      } catch (err) {
         navigate("/home");
       }
     }
-    getUser()
-  }, [])
+    getUser();
+  }, []);
+
+  function descriptionEdit(){
+    if (username === loggedInUser) {
+      return (
+        <div onClick={() => setIsOpenDescription(true)} className="description-edit">
+          <EditIcon className="icon"/>
+        </div>
+      );
+    }
+  }
 
   return (
     <div className="profile-page">
       <div className="profile-content">
         <div className="profile-header profile-box">
-          <div className="profile-avatar" style={{backgroundImage: `url("${user.avatarUrl || "https://raw.githubusercontent.com/mwisniewski00/Chess/main/client/public/logo512.png"}")`}}></div>
+          <div
+            className="profile-avatar"
+            style={{
+              backgroundImage: `url("${
+                user.avatarUrl ||
+                "https://raw.githubusercontent.com/mwisniewski00/Chess/main/client/public/logo512.png"
+              }")`,
+            }}
+          ></div>
           <div className="profile-details">
             <div className="profile-name"> {user.username} </div>
-            <div className="profile-title"> Grand Master </div>
             <div className="profile-status-bar">
               <table>
                 <tbody>
                   <tr>
-                    <td><AccessAlarmIcon className="icon"/></td>
-                    <td><CalendarMonthIcon className="icon"/></td>
-                    <td><ScoreboardIcon className="icon"/></td>
+                    <td>
+                      <AccessAlarmIcon className="icon" />
+                    </td>
+                    <td>
+                      <CalendarMonthIcon className="icon" />
+                    </td>
+                    <td>
+                      <ScoreboardIcon className="icon" />
+                    </td>
                   </tr>
                   <tr>
                     <td>{calculateTime(user.lastLoginDate)}</td>
@@ -111,40 +143,34 @@ const Profile: React.FC = () => {
               </table>
             </div>
           </div>
-          <div onClick={() => setIsOpen(true)} className="profile-edit"><EditIcon className="icon"/> Edit </div>
         </div>
 
         <div className="profile-section profile-box">
           <div className="profile-tabs-menu">
-            <Tabs 
-            value={value} 
-            onChange={handleChange} 
-            textColor="inherit"
-            indicatorColor="primary"
-            centered 
-            variant="fullWidth">
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              textColor="inherit"
+              indicatorColor="primary"
+              centered
+              variant="fullWidth"
+            >
               <Tab label="Description" {...a11yProps(0)} />
               <Tab label="Statistics" {...a11yProps(1)} />
               <Tab label="Game History" {...a11yProps(2)} />
             </Tabs>
           </div>
           <TabPanel value={value} index={0}>
-            {user.description} 
+            {user.description} {descriptionEdit()}
           </TabPanel>
-          <TabPanel value={value} index={1}>
-            
-          </TabPanel>
-          <TabPanel value={value} index={2}>
-            
-          </TabPanel>
+          <TabPanel value={value} index={1}></TabPanel>
+          <TabPanel value={value} index={2}></TabPanel>
         </div>
-    
       </div>
 
-      <EditUserModal isOpen={isOpen} setIsOpen={setIsOpen} userData={user}/>
-
+      <EditDescriptionModal isOpen={isOpenDescription} setIsOpen={setIsOpenDescription} userData={user} />
     </div>
-  )
-}
+  );
+};
 
 export default Profile;
